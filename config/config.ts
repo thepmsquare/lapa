670a4666700e633032c7439a1b683b85f7a8f544
thepmsquare <impmmmm@gmail.com>
2024-03-24 11:41:42 +0530

    remove unused config

--- config/config.ts
+++ config/config.ts
@@ -2,7 +2,5 @@ import Config from "../types/Config";
 
 const config: Config = {
   defaultThemeState: "dark",
-  defaultFont: "fira mono",
-  defaultFontHeadings: "roboto slab",
 };
 export default config;
--- types/Config.ts
+++ types/Config.ts
@@ -1,7 +1,5 @@
 import ThemeState from "./ThemeState";
 type Config = {
   defaultThemeState: ThemeState;
-  defaultFont: string;
-  defaultFontHeadings: string;
 };
 export default Config;
