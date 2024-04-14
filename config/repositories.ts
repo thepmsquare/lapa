21016a0097e90edb44a510b16adf9a63e309c7bf
AmishPalkar <87766719+B21amish@users.noreply.github.com>
2024-04-14 23:55:20 +0530

    Added lapaChat repo.

--- config/repositories.ts
+++ config/repositories.ts
@@ -228,6 +228,18 @@ const repositories: Repositories = [
     previewLink: null,
     programmingLanguage: "Python",
   },
+  {
+    id: uuid(),
+    repoName: "lapaChat",
+    latestVersion: null,
+    repoOwner: "Amish Palkar",
+    sourceCodeLink: {
+      value: "https://github.com/B21amish/lapaChat",
+      isPrivate: false,
+    },
+    previewLink: null,
+    programmingLanguage: "Node.js",
+  },
 ];
 
 export default repositories;
