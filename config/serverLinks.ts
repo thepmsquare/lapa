58625cd7996dd1d7dc24b9fe9c7445b487a14e4a
thepmsquare <impmmmm@gmail.com>
2024-04-26 15:58:03 +0530

    update link for pi

--- config/serverLinks.ts
+++ config/serverLinks.ts
@@ -1,21 +1,22 @@
 import type { ServerLinks } from "@/types/ServerLinks";
 import { v4 as uuid } from "uuid";
+
 const serverLinks: ServerLinks = [
-  { id: uuid(), component: "pgadmin", link: "http://43.224.175.127:10101/" },
+  { id: uuid(), component: "pgadmin", link: "http://111.125.236.217:10101/" },
   {
     id: uuid(),
     component: "lapa_database",
-    link: "http://43.224.175.127:10010/docs/",
+    link: "http://111.125.236.217:10010/docs/",
   },
   {
     id: uuid(),
     component: "lapa_authentication",
-    link: "http://43.224.175.127:10011/docs/",
+    link: "http://111.125.236.217:10011/docs/",
   },
   {
     id: uuid(),
     component: "lapa_file_store",
-    link: "http://43.224.175.127:10100/docs/",
+    link: "http://111.125.236.217:10100/docs/",
   },
 ];
 
