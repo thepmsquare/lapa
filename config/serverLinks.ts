62475ff5e0112d18fb431be6876bcc302852f8b7
thepmsquare <impmmmm@gmail.com>
2024-07-01 21:28:37 +0530

    https

--- config/serverLinks.ts
+++ config/serverLinks.ts
@@ -6,17 +6,17 @@ const serverLinks: ServerLinks = [
   {
     id: uuid(),
     component: "lapa_database",
-    link: "http://111.125.236.217:10010/docs/",
+    link: "https://111.125.236.217:10010/docs/",
   },
   {
     id: uuid(),
     component: "lapa_authentication",
-    link: "http://111.125.236.217:10011/docs/",
+    link: "https://111.125.236.217:10011/docs/",
   },
   {
     id: uuid(),
     component: "lapa_file_store",
-    link: "http://111.125.236.217:10100/docs/",
+    link: "https://111.125.236.217:10100/docs/",
   },
 ];
 
