7023a3b1cf0bd0125f28ab77f13f9d0bfb035f2e
thepmsquare <impmmmm@gmail.com>
2024-07-05 10:35:34 +0530

    Update serverLinks.ts

--- config/serverLinks.ts
+++ config/serverLinks.ts
@@ -2,21 +2,22 @@ import type { ServerLinks } from "@/types/ServerLinks";
 import { v4 as uuid } from "uuid";
 
 const serverLinks: ServerLinks = [
-  { id: uuid(), component: "pgadmin", link: "http://111.125.236.217:10101/" },
+  { id: uuid(), component: "server landing page", link: "https://lapa.live/" },
+  { id: uuid(), component: "pgadmin", link: "http://lapa.live:10101/" },
   {
     id: uuid(),
     component: "lapa_database",
-    link: "https://111.125.236.217:10010/docs/",
+    link: "https://lapa.live:10010/docs/",
   },
   {
     id: uuid(),
     component: "lapa_authentication",
-    link: "https://111.125.236.217:10011/docs/",
+    link: "https://lapa.live:10011/docs/",
   },
   {
     id: uuid(),
     component: "lapa_file_store",
-    link: "https://111.125.236.217:10100/docs/",
+    link: "https://lapa.live:10100/docs/",
   },
 ];
 
